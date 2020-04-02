e8d7ef24c66efd9bedd4a9fdb44111f94dc119ad
Guillaume180 <43747099+Guillaume180@users.noreply.github.com>
2020-04-02 13:17:55 +0800

    Changes

---
+++ gpg_gdg_230/Assets/scripts/MenuSetting.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MenuSetting : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+
+    Resolution[] resolutions;
+
+    public Dropdown resolutionsDropDown;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionsDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionsDropDown.AddOptions(options);
+        resolutionsDropDown.value = currentResolutionIndex;
+        resolutionsDropDown.RefreshShownValue();
+    }
+
+    public void Volume (float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolutions (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
---
+++ gpg_gdg_230/Assets/scripts/SelectPurchase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPurchase : MonoBehaviour
+{
+    public int purchaseNumber;
+
+    public void SelectPurchaseNumber()
+    {
+        FakeMicrotransactions.whichPurchaseOne = purchaseNumber;
+    }
+}
