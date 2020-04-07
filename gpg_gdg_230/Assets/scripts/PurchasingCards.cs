760e4ff3064baac3917330db2518e6c22b89cbe1
milanm2121 <55521009+milanm2121@users.noreply.github.com>
2020-04-07 10:26:23 +0800

    added card bying function

--- gpg_gdg_230/Assets/scripts/PurchasingCards.cs
+++ gpg_gdg_230/Assets/scripts/PurchasingCards.cs
@@ -6,10 +6,24 @@ public class PurchasingCards : MonoBehaviour
 {
 
    public static int cardcost;
+    public collection collection;
 
    public void PurcahseTheCards()
     {
         cardcost = 250;
-        FakeMicrotransactions.staticIngameCoins -= cardcost;
+        if (cardcost >= FakeMicrotransactions.staticIngameCoins)
+        {
+            FakeMicrotransactions.staticIngameCoins -= cardcost;
+
+            for (int i = 0; 6 < i; i++)
+            {
+                static_collections.Collection[Random.Range(0, 151)] += 1;
+            }
+            temp_collection x = new temp_collection();
+            x.temp_collection_save();
+            collection.load_cards();
+        }
+        else
+            Debug.Log("comeback when you get some more money");
     }
 }
