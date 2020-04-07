95871b0ad256e80c779dc6a704aa256e3fb315cb
Guillaume180 <43747099+Guillaume180@users.noreply.github.com>
2020-04-07 08:30:30 +0800

    change

--- gpg_gdg_230/Assets/scripts/FakeMicrotransactions.cs
+++ gpg_gdg_230/Assets/scripts/FakeMicrotransactions.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//This script is to make a fake microtransaction to our game. 
+//I am making it this mock transaction as close as it needs to be to the real deal.
+//Though this is the proper coding for this.
+//Guillaume Blanchard
 public class FakeMicrotransactions : MonoBehaviour
 {
 
+    //making the coins  static so that other scripts can call on it in order to buy cards
+    public static int staticIngameCoins;
     public int ingameCoins;
+
+    public Text coinText;
 
     public static int whichPurchaseOne;
     public int[] purchases;
@@ -21,29 +29,44 @@ public class FakeMicrotransactions : MonoBehaviour
     public InputField cardCCV;
 
 
+    void Update()
+    {
+        if (ingameCoins != staticIngameCoins)
+        {
+            ingameCoins = staticIngameCoins;
+            coinText.text = ingameCoins.ToString();
+        }
+    }
+
     //I fix this later in order to see if something has been put in into the card detail.
     public void CheckForPurchase()
     {
-        if (cardName.text.Length >= 4 && cardNumber.text.Length == 12 && cardExpiryMonth.text.Length == 2 & cardExpiryYear.text.Length == 2 && cardCCV.text.Length == 3)
-        {
+        if (cardName.text.Length >= 4)
+            if (cardNumber.text.Length >= 12)
+                if (cardExpiryMonth.text.Length == 2)
+                    if (cardExpiryYear.text.Length == 2)
+                        if (cardCCV.text.Length == 3)
+                        {
 
-            Debug.Log(whichPurchaseOne);
-            for (int i = 0; i < 6; i++)
-            {
+                            Debug.Log(whichPurchaseOne);
+                            for (int i = 0; i < 6; i++)
+                            {
 
-                if (whichPurchaseOne == i)
-                {
-                    ingameCoins += purchases[i];
-                    Debug.Log("it works");
-                    purchaseScreen.SetActive(false);
-                    cardName.text = " ";
-                    cardNumber.text = " ";
-                    cardExpiryMonth.text = " ";
-                    cardExpiryYear.text = " ";
-                    cardCCV.text = " ";
-                }
-            }
-        }
+                                if (whichPurchaseOne == i)
+                                {
+                                    staticIngameCoins += purchases[i];
+                                    ingameCoins = staticIngameCoins;
+                                    Debug.Log("it works");
+                                    purchaseScreen.SetActive(false);
+                                    cardName.text = " ";
+                                    cardNumber.text = " ";
+                                    cardExpiryMonth.text = " ";
+                                    cardExpiryYear.text = " ";
+                                    cardCCV.text = " ";
+                                    coinText.text = ingameCoins.ToString();
+                                }
+                            }
+                        }
 
     }
 }
---
+++ gpg_gdg_230/Assets/scripts/PurchasingCards.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchasingCards : MonoBehaviour
+{
+
+   public static int cardcost;
+
+   public void PurcahseTheCards()
+    {
+        cardcost = 250;
+        FakeMicrotransactions.staticIngameCoins -= cardcost;
+    }
+}
