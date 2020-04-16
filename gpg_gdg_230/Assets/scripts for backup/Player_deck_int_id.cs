5d50b88f562a8d359339e353937d36ef4899d980
milanm2121 <55521009+milanm2121@users.noreply.github.com>
2020-04-16 20:00:24 +0800

    menu done

---
+++ gpg_gdg_230/Assets/scripts for backup/Player_deck_int_id.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_deck_int_id : MonoBehaviour
+{
+	public List<int> deck = new List<int>();
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Update is called once per frame
+	public void loadDeck()
+	{
+		if (SceneManager.GetActiveScene().name == "PVPTableScene")
+		{
+			PlayerDeck activeDeck = GameObject.Find("Deck Panel").GetComponent<PlayerDeck>();
+            activeDeck.loadDeack(deck);
+		}
+	}
+}
